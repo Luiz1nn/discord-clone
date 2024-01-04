9c445b350325a452e25d8e64031810b8be38b59a
Luis Fernando <luisfernandobr2003@gmail.com>
2024-01-04 16:53:43 -0300

    --amend

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
+import { cn } from '~/lib/utils'
 import { ThemeProvider } from '~/components/providers/theme-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -19,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
+        <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
