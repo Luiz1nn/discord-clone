8b188886dc8c189bbe517033650bcd5c1a11ddd9
Luis Fernando <luisfernandobr2003@gmail.com>
2024-01-23 10:50:03 -0300

    feat(chat-messages): add loading component and error component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { Member } from '@prisma/client'
+import { useChatQuery } from '~/hooks/use-chat-query'
 
 import { ChatWelcome } from './chat-welcome'
+import { Loader2, ServerCrash } from 'lucide-react'
 
 type Props = {
   name: string
@@ -25,6 +29,37 @@ export const ChatMessages = ({
   socketUrl,
   type,
 }: Props) => {
+  const queryKey = `chat:${chatId}`
+
+  const { status } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+  })
+
+  if (status === 'loading') {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    )
+  }
+
+  if (status === 'error') {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
